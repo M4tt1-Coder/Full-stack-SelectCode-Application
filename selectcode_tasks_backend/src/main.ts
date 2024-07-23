c25ab7d4318cfb6ab560940e731f330b6772aa3e
M4tt1-Coder <geimat75@gmail.com>
2024-07-24 01:27:19 +0200

    docker implementation for the backend service

--- selectcode_tasks_backend/src/main.ts
+++ selectcode_tasks_backend/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+//import { initTRPC } from '@trpc/server';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
