7a5888ff997d9442b0dc81344de20b8201e27318
M4tt1-Coder <geimat75@gmail.com>
2024-08-01 15:50:05 +0200

    initial commit frontend branch

--- selectcode_tasks_backend/src/main.ts
+++ selectcode_tasks_backend/src/main.ts
@@ -4,7 +4,7 @@ import { TrpcRouter } from './trpc/trpc.router';
 
 // https://www.tomray.dev/nestjs-nextjs-trpc [for trpc setup]
 
-// TODO - Possibly add tests later on
+// TODO - Possibly add tests later on -> {utility functions in the entity files; trpc / service tests after the frontend is finished}
 
 /**
  * Starts the server
